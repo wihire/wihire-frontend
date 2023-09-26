2847820982e9523254712d16288816a28612d130
unknown <mhdtarmiz11@gmail.com>
2023-09-26 23:46:59 +0700

    slicing onpage

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,97 +1,37 @@
 import Button from '@/components/elements/Button';
 import Container from '@/components/elements/Container';
-import FormControl from '@/components/elements/FormControl';
-import Logo from '@/components/elements/Logo';
-import LogoText from '@/components/elements/LogoText';
-import Select from '@/components/elements/Select';
-import TextInput from '@/components/elements/TextInput';
+import Model from '@/assets/images/ilustrations/onboard.png';
+import Bulat from '@/assets/images/ilustrations/bulat.png';
+import Image from 'next/image';
 
 const Home = () => (
   <Container>
-    <Logo size="sm" />
-    <Logo />
-    <Logo size="lg" />
-    <Logo size="xl" />
-
-    <LogoText size="sm" />
-    <LogoText />
-    <LogoText size="lg" />
-    <LogoText size="xl" />
-
-    <Button>Login</Button>
-    <Button className="btn-neutral">Neutral</Button>
-    <Button className="btn-secondary">Secondary</Button>
-    <Button className="btn-accent">Accent</Button>
-    <Button className="btn-ghost">Ghost</Button>
-    <Button className="btn-link">Link</Button>
-
-    <TextInput placeholder="Username" />
-
-    <FormControl
-      htmlFor="name"
-      label="Name"
-      labelAlt="This is a name"
-      description="This is a description"
-      descriptionAlt="1/100"
-    >
-      <TextInput id="name" placeholder="Name" />
-    </FormControl>
-    <FormControl
-      htmlFor="address"
-      label="Username"
-      labelAlt="This is a address"
-      description="This is a description"
-      descriptionAlt="1/100"
-      error="This is an error"
-    >
-      <TextInput id="address" placeholder="Address" />
-    </FormControl>
-    <TextInput type="file" />
-
-    <Select
-      defaultValue="Vue"
-      options={[
-        {
-          value: 'React',
-          label: 'React'
-        },
-        {
-          value: 'Vue',
-          label: 'Vue'
-        },
-        {
-          value: 'Angular',
-          label: 'Angular'
-        }
-      ]}
-    />
-
-    <FormControl
-      htmlFor="language"
-      label="Language"
-      labelAlt="This is a name"
-      description="This is a description"
-      descriptionAlt="1/100"
-    >
-      <Select
-        id="language"
-        placeholder="Pick a framework"
-        options={[
-          {
-            value: 'React',
-            label: 'React'
-          },
-          {
-            value: 'Vue',
-            label: 'Vue'
-          },
-          {
-            value: 'Angular',
-            label: 'Angular'
-          }
-        ]}
-      />
-    </FormControl>
+    <div className="h-screen flex items-center relative">
+      <div className="fixed -mb-[650px] -ml-[100px]">
+        <Image src={Bulat} />
+      </div>
+      <div className="fixed rotate-180 ml-[1100px] mb-[550px]">
+        <Image src={Bulat} />
+      </div>
+      <div className="w-1/2 pl-20 ">
+        <div className="">
+          <h1 className="text-7xl text-primary font-bold mb-5">Find Your</h1>
+          <h2 className="text-5xl font-bold mb-5">Dream Job</h2>
+          <h3 className="text-2xl mb-5">
+            Unlock your full potential with your dream job or find the perfect candidate for your
+            company needs now
+          </h3>
+          <Button href="/register">JOIN NOW TO ACHIEVE YOUR OPPORTUNITY</Button>
+          <h1 className="my-2">or you can</h1>
+          <Button href="/login" className="btn-outline bg-white">
+            LOGIN TO YOUR EXISTING ACCOUNT
+          </Button>
+        </div>
+      </div>
+      <div class="w-1/2 pr-20 flex justify-center">
+        <Image src={Model} width={450} height={450} />
+      </div>
+    </div>
   </Container>
 );
 
