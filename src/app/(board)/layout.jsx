bf60ead187fe99d054bbee84f61e2dbbfea65430
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-09-26 20:08:26 +0700

    fix: update background color separated

--- src/app/(board)/layout.jsx
+++ src/app/(board)/layout.jsx
@@ -2,6 +2,8 @@ import Container from '@/components/elements/Container';
 import Navbar from '@/components/parts/Navbar';
 import SidebarBoardUser from '@/components/parts/Sidebar/SidebarBoardUser';
 
+import './styles.scss';
+
 const Layout = ({ children }) => (
   <>
     <Navbar />
