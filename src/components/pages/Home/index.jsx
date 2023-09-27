e0df040e3b88b5d1a7ff1a19ac77d6c9ac6a7617
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-09-27 22:11:49 +0700

    feat: add fadein animation

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,37 +1,41 @@
-/* eslint-disable tailwindcss/enforces-negative-arbitrary-values */
+/* eslint-disable max-len */
 import Image from 'next/image';
 
-import Bulat from '@/assets/images/ilustrations/bulat.png';
-import Model from '@/assets/images/ilustrations/onboard.png';
+import mondelOnBoard from '@/assets/images/ilustrations/onboard.png';
+import CircleDecoration from '@/assets/vectors/circle-decoration.svg';
 import Button from '@/components/elements/Button';
 import Container from '@/components/elements/Container';
+import Logo from '@/components/elements/Logo';
+import Text from '@/components/elements/Text';
+import Header from '@/components/parts/Home/Home';
 
 const Home = () => (
   <Container>
-    <div className="relative flex h-screen items-center">
-      <div className="fixed -mb-[650px] -ml-[100px]">
-        <Image src={Bulat} alt="" />
+    <div className="min-h-d-screen relative flex items-center">
+      <CircleDecoration className="fixed bottom-0 left-0 -translate-x-1/4 translate-y-1/4 -rotate-90" />
+      <CircleDecoration className="fixed right-0 top-0 -translate-y-1/4 translate-x-1/4 rotate-90" />
+
+      <Logo className="fixed top-6" />
+
+      <div className="w-1/2">
+        <Header />
+
+        <Text className="mb-10 mt-5 text-2xl">
+          Unlock your full potential with your dream job
+          <br />
+          or find the perfect candidate for your company needs now
+        </Text>
+        <Button href="/register">JOIN NOW TO ACHIEVE YOUR OPPORTUNITY</Button>
+
+        <Text className="my-2">or you can</Text>
+
+        <Button href="/login" className="btn-outline bg-white">
+          LOGIN TO YOUR EXISTING ACCOUNT
+        </Button>
       </div>
-      <div className="fixed mb-[550px] ml-[1100px] rotate-180">
-        <Image src={Bulat} alt="" />
-      </div>
-      <div className="w-1/2 pl-20 ">
-        <div className="">
-          <h1 className="mb-5 text-7xl font-bold text-primary">Find Your</h1>
-          <h2 className="mb-5 text-5xl font-bold">Dream Job</h2>
-          <h3 className="mb-5 text-2xl">
-            Unlock your full potential with your dream job or find the perfect candidate for your
-            company needs now
-          </h3>
-          <Button href="/register">JOIN NOW TO ACHIEVE YOUR OPPORTUNITY</Button>
-          <h1 className="my-2">or you can</h1>
-          <Button href="/login" className="btn-outline bg-white">
-            LOGIN TO YOUR EXISTING ACCOUNT
-          </Button>
-        </div>
-      </div>
-      <div class="flex w-1/2 justify-center pr-20">
-        <Image src={Model} width={450} height={450} alt="" />
+
+      <div class="flex w-1/2 justify-center">
+        <Image src={mondelOnBoard} width={500} height={500} alt="Onboard model" />
       </div>
     </div>
   </Container>
