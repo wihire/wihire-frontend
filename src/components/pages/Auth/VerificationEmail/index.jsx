aef265c5a563b16a37eca9d58b5ee056f61f1ef4
erwinmareto <100197413+erwinmareto@users.noreply.github.com>
2023-09-29 11:12:45 +0700

    feat: slicing email verification page (#4)
    tadinya buat countdown pake function pisah tapi kena error ESlint jadi
    disatuin langsung di useEfect
    
    Errornya:
    `
    14:3 Error: The 'countdown' function makes the dependencies of useEffect
    Hook (at line 22) change on every render. Move it inside the useEffect
    callback. Alternatively, wrap the definition of 'countdown' in its own
    useCallback() Hook. react-hooks/exhaustive-deps`
    
    ---------
    
    Co-authored-by: Jovanka Samudra <samudrajovanka20@gmail.com>

--- src/components/pages/Auth/VerificationEmail/index.jsx
+++ src/components/pages/Auth/VerificationEmail/index.jsx
@@ -1,3 +1,34 @@
-const VerificationEmail = () => <div>VerificationEmail</div>;
+import Image from 'next/image';
+
+import verifyEmailArt from '@/assets/images/illustrations/verified-email.png';
+import Container from '@/components/elements/Container';
+import Text from '@/components/elements/Text';
+import SendVerification from '@/components/parts/VerificationEmail/SendVerification';
+import { getCookie } from '@/lib/cookies';
+
+const VerificationEmail = () => {
+  const userEmail = getCookie('email');
+
+  return (
+    <Container className="grid justify-items-center gap-5 rounded-md border border-gray-100 p-14">
+      <Image src={verifyEmailArt} width={300} height={300} alt="verify-email" />
+
+      <Text as="h1" typography="h1" className="text-center">
+        Verify Your Email Address
+      </Text>
+
+      <Text className="text-center">
+        We have sent an email to <strong>{userEmail}</strong>, <br />
+        Please follow the link provided there to verify your email
+      </Text>
+
+      <div>
+        <Text className="text-center">Resend email?</Text>
+
+        <SendVerification email={userEmail} />
+      </div>
+    </Container>
+  );
+};
 
 export default VerificationEmail;
