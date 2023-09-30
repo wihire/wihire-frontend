3a4395c47b8ad4c2e0b35dc979cbf765b8405ae4
Jovanka Samudra <51015175+samudrajovanka@users.noreply.github.com>
2023-09-30 13:43:01 +0700

    feat: create saved jobs page (#17)

--- src/app/(board)/applications/page.jsx
+++ src/app/(board)/applications/page.jsx
@@ -1,9 +1,19 @@
 import { Hydrate, dehydrate } from '@tanstack/react-query';
 
 import Applications from '@/components/pages/Applications';
+import generateMetadata from '@/lib/metadata';
 import { getQueryClient } from '@/lib/queryClient';
 import { getApplicationsKeys } from '@/query/applications';
 import { getApplications } from '@/repositories/applications';
+
+export const metadata = generateMetadata(
+  {
+    title: 'Applications'
+  },
+  {
+    withSuffix: true
+  }
+);
 
 const ApplicationsPage = async ({ searchParams }) => {
   const queryClient = getQueryClient();
