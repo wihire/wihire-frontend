ed973014963be0392cc2345dbbf78b7d634a51a5
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-10-01 18:23:37 +0700

    fix: resolve conflict

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-<<<<<<< HEAD
 import { useParams } from 'next/navigation';
 
 import About from '@/components/parts/Profile/About';
@@ -12,21 +11,23 @@ import Resume from '@/components/parts/Profile/Resume';
 import SalaryExpectation from '@/components/parts/Profile/SalaryExpetation';
 import ListSkill from '@/components/parts/Profile/SkillList';
 import WorkExperienceList from '@/components/parts/Profile/WorkExperienceList';
-=======
 import { useMemo } from 'react';
 
 import { useParams } from 'next/navigation';
 
 import ProfileCompany from '@/components/parts/ProfileCompany';
 import { ROLE } from '@/lib/constants/common';
->>>>>>> develop
 import { useProfile } from '@/query/profile';
 
 const Profile = () => {
   const params = useParams();
 
-<<<<<<< HEAD
-  const { data } = useProfile(params.userSlug);
+  const { data } = useProfile(params.profileSlug);
+  const profile = useMemo(() => data?.data?.data?.profile, [data]);
+
+  if (profile?.role === ROLE.COMPANY) {
+    return <ProfileCompany />;
+  }
 
   return (
     <div className="flex flex-col gap-5">
@@ -41,16 +42,6 @@ const Profile = () => {
       <ProjectList projects={data?.data?.data?.profile?.user?.projects} />
     </div>
   );
-=======
-  const { data } = useProfile(params.profileSlug);
-  const profile = useMemo(() => data?.data?.data?.profile, [data]);
-
-  if (profile?.role === ROLE.COMPANY) {
-    return <ProfileCompany />;
-  }
-
-  return <p>Profile</p>;
->>>>>>> develop
 };
 
 export default Profile;
