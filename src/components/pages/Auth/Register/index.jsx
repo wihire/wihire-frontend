5bc2dc72c1bbfba56e47336596dffab68dac6360
fajrinnurhakim <65064246+fajrinnurhakim@users.noreply.github.com>
2023-10-11 23:36:56 +0700

    feat(register): create responsive layout (#29)

--- src/components/pages/Auth/Register/index.jsx
+++ src/components/pages/Auth/Register/index.jsx
@@ -5,12 +5,15 @@ import RegisterAsCompany from '@/components/parts/Register/RegisterAsCompany';
 import RegisterAsUser from '@/components/parts/Register/RegisterAsUser';
 
 const Register = () => (
-  <div className="min-h-d-screen flex">
+  <div className="min-h-d-screen flex flex-col lg:flex-row">
     <RegisterAsCompany />
 
     <RegisterAsUser />
 
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 rounded-md bg-white p-5 shadow-md">
+    <div
+      className="bottom-6 mx-auto my-10 rounded-md bg-white p-5 shadow-md
+      lg:fixed lg:left-1/2 lg:my-5 lg:-translate-x-1/2"
+    >
       <Text typography="sm">
         You already have an account?{' '}
         <Link href="/login" className="font-bold text-primary underline">
